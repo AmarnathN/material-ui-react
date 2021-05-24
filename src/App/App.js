467c6a157d343d0a1ac27dc1316e9eb1dd88fc68
AmarnathN <vayuteja@gmail.com>
2021-05-24 15:32:37 +0530

    Start App

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidemenu from "../components/Sidemenu";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Sidemenu></Sidemenu>
+      <div>here we go</div>
+    </React.Fragment>
+  );
+}
+
+export default App;
