8f46079a308b51e0a9e13c485f6ced0ae5c1df8c
AmarnathN <vayuteja@gmail.com>
2021-05-27 13:09:42 +0530

    Adding styling to table

--- src/components/controlsHandlers/useTable.js
+++ src/components/controlsHandlers/useTable.js
@@ -1,9 +1,25 @@
-import { Table, TableCell, TableHead as MuiTableHead, TableRow } from "@material-ui/core";
+import { makeStyles, Table, TableCell, TableHead as MuiTableHead, TableRow } from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles((theme) => ({
+  table: {
+    marginTop: theme.spacing(2),
+    "& thead th": {
+      fontWeight: "600",
+      color: theme.palette.primary.main,
+      backgroundColor: theme.palette.primary.light,
+    },
+    "& tbody td": {
+      fontWeight: "300",
+    },
+    "& tbody tr:hover": { cursor: "pointer", backgroundColor: theme.palette.theme.light },
+  },
+}));
+
 const useTable = (records, headers) => {
+  const classes = useStyles();
   const TableContainer = (props) => {
-    return <Table>{props.children}</Table>;
+    return <Table className={classes.table}>{props.children}</Table>;
   };
 
   const TableHead = (props) => {
