96fdd5b7cb0537ec29cf511a199a7bc196d42bf2
AmarnathN <vayuteja@gmail.com>
2021-05-28 02:57:23 +0530

    Adding custom styling to Dialog Popop *****

--- src/components/controls/PopupDialog.js
+++ src/components/controls/PopupDialog.js
@@ -1,11 +1,19 @@
 import { Dialog, DialogContent, DialogTitle, Divider, makeStyles } from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles((theme) => ({
+  dialogWrapper: {
+    padding: theme.spacing(2),
+    position: "absolute",
+    top: theme.spacing(5),
+  },
+}));
+
 const PopupDialog = (props) => {
   const classes = useStyles();
   const { title, children, openPopup, setOpenPopup } = props;
   return (
-    <Dialog open={openPopup} maxWidth="md">
+    <Dialog open={openPopup} maxWidth="md" classes={{ paper: classes.dialogWrapper }}>
       <DialogTitle>{title}</DialogTitle>
       <Divider />
       <DialogContent>{children}</DialogContent>
